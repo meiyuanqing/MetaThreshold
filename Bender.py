61a6763c5e6c7e8ddbcd8399f9ab3f4b3bd5c98d
yuanqingmei <njumyq@outlook.com>
2021-03-16 18:02:47 +0800

    Use Bender method to derive metric threshold.

---
+++ Bender.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding:utf-8
+"""
+Author : Yuanqing Mei
+Date : 2021/3/16
+Time: 17:59
+File: Bender.py
+HomePage : http://github.com/yuanqingmei
+Email : dg1533019@smail.nju.edu.cn
+
+Use Bender method to derive metric threshold.
+"""
