1e194723af2605d1552136827e5c0a52b6274877
yuanqingmei <njumyq@outlook.com>
2021-04-13 11:36:32 +0800

    T2 = 0  # 20210411，若T2小于0，取0,   M.Borenstein[2009] P114

--- PooledMeta.py
+++ PooledMeta.py
@@ -83,7 +83,7 @@ def pooled_meta(t_dir="F:\\NJU\\MTmeta\\experiments\\pooled\\PoolingThresholds\\
             T2 = (Q - df) / C  # sample estimate of tau square
 
         if T2 < 0:
-            T2 = (- 1) * T2  # 20190719，若T2小于0，取相反数
+            T2 = 0  # 20210411，若T2小于0，取0,   M.Borenstein[2009] P114
 
         for i in range(study_number):
             random_weight[i] = 1 / (variance[i] + T2)  # random_weight 随机模型对应的权值
@@ -129,18 +129,148 @@ def pooled_meta(t_dir="F:\\NJU\\MTmeta\\experiments\\pooled\\PoolingThresholds\\
         d['fixedStdError'] = fixedStdError
         return d
 
+
+    def getEstimatedK0(effectSizeArray, mean):
+        centeredEffectSizeArray = []
+        absoluteCenteredEffectSizeArray = []
+        size = len(effectSizeArray)
+        for i in range(size):
+            centeredEffectSizeArray.append(effectSizeArray[i] - mean)
+            absoluteCenteredEffectSizeArray.append(np.abs(effectSizeArray[i] - mean))
+        sortedArray = sorted(absoluteCenteredEffectSizeArray)
+        rank = {sortedArray[0]: 1}  # return a dict
+        initialRankValue = 1
+        predValue = sortedArray[0]
+        for i in range(size):
+            if sortedArray[i] > predValue:
+                predValue = sortedArray[i]
+                initialRankValue += 1
+            rank[sortedArray[i]] = initialRankValue
+        finalRank = []
+        for i in range(size):
+            if centeredEffectSizeArray[i] < 0:
+                finalRank.append((-1) * rank[absoluteCenteredEffectSizeArray[i]])
+            else:
+                finalRank.append(rank[absoluteCenteredEffectSizeArray[i]])
+        gamma = finalRank[size - 1] + finalRank[0]
+        SumPositiveRank = 0
+        for i in range(size):
+            if finalRank[i] < 0:
+                continue
+            SumPositiveRank = SumPositiveRank + finalRank[i]
+        R0 = int(gamma + 0.5) - 1
+        temp = (4 * SumPositiveRank - size * (size + 1)) / (2 * size - 1)
+        L0 = int(temp + 0.5)
+        if R0 < 0:
+            R0 = 0
+        if L0 < 0:
+            L0 = 0
+        return R0, L0
+
+    # Duval and Tweedie's trim and fill method
+    def trimAndFill(effect_size, variance, isAUC):
+        effectSizeArray = effect_size
+        varianceArray = variance
+        size = len(effect_size)
+        # 检查是否需要切换方向，因为trim and fill方法假设miss most negative的研究
+        flipFunnel = 0
+        metaAnalysisForFlip = fixed_effect_meta_analysis(effectSizeArray, varianceArray)
+        meanForFlip = metaAnalysisForFlip["fixedMean"]
+
+        tempSorted = sorted(effectSizeArray)
+        min = tempSorted[0] - meanForFlip
+        max = tempSorted[-1] - meanForFlip
+
+        if np.abs(min) > np.abs(max):
+            flipFunnel = 1
+            for i in range(size):
+                effectSizeArray[i] = (-1) * effectSizeArray[i]
+
+        # 按effect size排序
+        merge = []
+        for i in range(size):
+            merge.append([effect_size[i], variance[i]])
+        sortedMerge = sorted(merge)
+        OrignalEffectSizeArray = []
+        OrignalVarianceArray = []
+        for i in range(len(sortedMerge)):
+            OrignalEffectSizeArray.append(sortedMerge[i][0])
+            OrignalVarianceArray.append(sortedMerge[i][1])
+        # 迭代算法，估算k0
+        metaAnalysisResult = fixed_effect_meta_analysis(OrignalEffectSizeArray, OrignalVarianceArray)
+        mean = metaAnalysisResult["fixedMean"]
+        RL = getEstimatedK0(OrignalEffectSizeArray, mean)
+        R0 = RL[0]
+        L0 = RL[1]
+        k0 = L0  # 默认的情况利用L0来估算k0
+        if (k0 == 0) or (k0 > size):
+            result = random_effect_meta_analysis(effect_size, variance)
+            result["k0"] = k0
+            return result
+        trimmedMean = mean
+        change = 1
+        count = 0
+        while change and (size - k0) > 2 and (count < 1000):
+            count += 1
+            upperBound = size - k0 - 1
+            trimmedEffectSizeArray = []
+            trimmedVarianceArray = []
+            for i in range(upperBound):
+                trimmedEffectSizeArray.append(OrignalEffectSizeArray[i])
+                trimmedVarianceArray.append(OrignalVarianceArray[i])
+            trimmedMetaAnalysisResult = fixed_effect_meta_analysis(trimmedEffectSizeArray, trimmedVarianceArray)
+            trimmedMean = trimmedMetaAnalysisResult["fixedMean"]
+            trimmedR0_L0 = getEstimatedK0(OrignalEffectSizeArray, trimmedMean)
+            trimmedR0 = trimmedR0_L0[0]
+            trimmedL0 = trimmedR0_L0[1]
+            k1 = trimmedL0
+            if k1 == k0:
+                change = 0
+            k0 = k1
+        filledEffectSizeArray = []
+        filledVarianceArray = []
+
+        for j in range(k0):
+            imputedEffectSize = 2 * trimmedMean - OrignalEffectSizeArray[size - j - 1]
+            imputedVariance = OrignalVarianceArray[size - j - 1]
+            filledEffectSizeArray.append(imputedEffectSize)
+            filledVarianceArray.append(imputedVariance)
+        fullEffectSizeArray = filledEffectSizeArray
+        fullVarianceArray = filledVarianceArray
+        fullEffectSizeArray.extend(OrignalEffectSizeArray)
+        fullVarianceArray.extend(OrignalVarianceArray)
+        if flipFunnel:
+            newSize = len(fullEffectSizeArray)
+            for i in range(newSize):
+                fullEffectSizeArray[i] = -1 * fullEffectSizeArray[i]
+
+        if isAUC:
+            # AUC应该在0到1之间，否则有错
+            filteredFullEffectSizeArray = []
+            filteredFullVarianceArray = []
+            for i in range(len(fullEffectSizeArray)):
+                if fullEffectSizeArray[i] < 0:
+                    continue
+                if fullEffectSizeArray[i] > 1:
+                    continue
+                filteredFullEffectSizeArray.append(fullEffectSizeArray[i])
+                filteredFullVarianceArray.append(fullVarianceArray[i])
+            result = random_effect_meta_analysis(filteredFullEffectSizeArray, filteredFullVarianceArray)
+            finalk0 = len(filteredFullEffectSizeArray) - len(OrignalEffectSizeArray)
+        else:
+            result = random_effect_meta_analysis(fullEffectSizeArray, fullVarianceArray)
+            finalk0 = len(fullEffectSizeArray) - len(OrignalEffectSizeArray)
+        result["k0"] = finalk0
+        result["flipFunnel"] = flipFunnel
+        return result
+
     metric_dir = t_dir
     meta_dir = m_dir
     os.chdir(metric_dir)
     print(os.getcwd())
 
-    # 显示所有行
     pd.set_option('display.max_rows', None)
-
-    # 显示所有列
     pd.set_option('display.max_columns', None)
-
-    # the item of row of dataframe
     pd.set_option('display.width', 5000)
 
     # read one of file to get the metric names for meta-analysis
@@ -179,6 +309,8 @@ def pooled_meta(t_dir="F:\\NJU\\MTmeta\\experiments\\pooled\\PoolingThresholds\\
             if len(df[df["metric"] == metric].loc[:, file[:-5]].values) == 0:
                 continue
 
+            print("the ", metric, " values is ", float(df[df["metric"] == metric].loc[:, file[:-5]].values[0]))
+            print("the variance is ", float(df[df["metric"] == metric].loc[:, file[:-5] + "_stdError"].values[0]))
             threshold_effect_size.append(float(df[df["metric"] == metric].loc[:, file[:-5]].values[0]))
             threshold_variance.append(float(df[df["metric"] == metric].loc[:, file[:-5] + "_stdError"].values[0]))
 
@@ -195,19 +327,32 @@ def pooled_meta(t_dir="F:\\NJU\\MTmeta\\experiments\\pooled\\PoolingThresholds\\
                 np.array(metaThreshold[metaThreshold["EffectSize"] > 0].loc[:, "EffectSize"]),
                 np.array(metaThreshold[metaThreshold["EffectSize"] > 0].loc[:, "Variance"]))
 
+            adjusted_result = trimAndFill(
+                np.array(metaThreshold[metaThreshold["EffectSize"] > 0].loc[:, "EffectSize"]),
+                np.array(metaThreshold[metaThreshold["EffectSize"] > 0].loc[:, "Variance"]), 0)
+
             with open(meta_dir + "Pooled_thresholds.csv", 'a+', encoding="utf-8", newline='') as f:
                 writer_f = csv.writer(f)
                 if os.path.getsize(meta_dir + "Pooled_thresholds.csv") == 0:
                     writer_f.writerow(
                         ["metric", "Pooled_meta_threshold", "Pooled_meta_threshold_stdError", "LL_CI", "UL_CI",
-                         "ZValue", "pValue_Z", "Q", "df", "pValue_Q", "I2", "tau",
-                         "LL_ndPred", "UL_tdPred", "number_of_effect_size"])
+                         "ZValue", "pValue_Z", "Q", "df", "pValue_Q", "I2", "tau", "LL_ndPred", "UL_tdPred",
+                         "number_of_effect_size",
+                         "k_0", "Pooled_meta_threshold_adjusted", "Pooled_meta_threshold_stdError_adjusted",
+                         "LL_CI_adjusted", "UL_CI_adjusted", "pValue_Z_adjusted", "Q_adjusted", "df_adjusted",
+                         "pValue_Q_adjusted", "I2_adjusted", "tau_adjusted", "LL_ndPred_adjusted",
+                         "UL_ndPred_adjusted"])
                 writer_f.writerow([metric, resultMetaAnalysis["mean"], resultMetaAnalysis["stdError"],
                                    resultMetaAnalysis["LL_CI"], resultMetaAnalysis["UL_CI"],
                                    resultMetaAnalysis["ZValue"], resultMetaAnalysis["pValue_Z"],
                                    resultMetaAnalysis["Q"], resultMetaAnalysis["df"], resultMetaAnalysis["pValue_Q"],
                                    resultMetaAnalysis["I2"], resultMetaAnalysis["tau"], resultMetaAnalysis["LL_ndPred"],
-                                   resultMetaAnalysis["UL_tdPred"], len(threshold_effect_size)])
+                                   resultMetaAnalysis["UL_tdPred"], len(threshold_effect_size),
+                                   adjusted_result["k0"], adjusted_result["mean"], adjusted_result["stdError"],
+                                   adjusted_result["LL_CI"], adjusted_result["UL_CI"], adjusted_result["pValue_Z"],
+                                   adjusted_result["Q"], adjusted_result["df"], adjusted_result["pValue_Q"],
+                                   adjusted_result["I2"], adjusted_result["tau"], adjusted_result["LL_ndPred"],
+                                   adjusted_result["UL_ndPred"]])
 
         except Exception as err1:
             print(err1)
